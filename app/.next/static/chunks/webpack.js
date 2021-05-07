c3372319e60e23af00ddef355c9f7a3d9aa189ee
Kevinpak <kevinpak@yandex.ru>
2021-05-07 18:03:13 +0300

    Public app integration

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b022fe190041741d0ad";
+/******/ 	var hotCurrentHash = "2dbcccdac91277db30c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
